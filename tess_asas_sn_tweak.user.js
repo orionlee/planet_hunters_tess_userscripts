6f97cca14eafa57734dcb83c7cdcd2c7238c36e7
orionlee <orionlee@yahoo.com>
2021-08-08 11:38:45 -0700

    ASAS-SN : make variable type link more visible

--- tess_asas_sn_tweak.user.js
+++ tess_asas_sn_tweak.user.js
@@ -1,26 +1,55 @@
 // ==UserScript==
 // @name        TESS - ASAS-SN Variable tweak
 // @namespace   astro.tess
-// @match       https://asas-sn.osu.edu/variables
+// @match       https://asas-sn.osu.edu/variables*
 // @grant       GM_addStyle
-// @version     1.0
+// @version     1.1
 // @author      orionlee
 // @description
 // ==/UserScript==
 
-if (location.search) {
-  // cae the page has query string params, i.e., a search is done
-  // make search result above the fold
+//
+// Search UI
+//
 
-  // - hide decorative UIs that take up vertical space
+function tweakSearchUI() {
+  if (location.pathname === '/variables' && location.search) {
+    // cae the page has query string params, i.e., a search is done
+    // make search result above the fold
+
+    // - hide decorative UIs that take up vertical space
+    GM_addStyle(`
+  .project-banner {
+    display: none;
+  }
+  `);
+
+    // - hide the search form to surface the result
+    document.querySelector('button.panel-header-btn').click();
+    // setTimeout(() => {
+    // }, 1000);
+  }
+}
+tweakSearchUI();
+
+
+//
+// Object Detail UI
+//
+
+
+function isObjectDetailPage() {
+  return location.pathname.match(/^\/variables\/.+/);
+}
+
+function tweakObjectDetail() {
+  if (!isObjectDetailPage()) {
+    return;
+  }
   GM_addStyle(`
-.project-banner {
-  display: none;
+a#variable-db-atlas-link {
+    text-decoration: underline;
 }
 `);
-
-  // - hide the search form to surface the result
-  document.querySelector('button.panel-header-btn').click();
-  // setTimeout(() => {
-  // }, 1000);
 }
+tweakObjectDetail();

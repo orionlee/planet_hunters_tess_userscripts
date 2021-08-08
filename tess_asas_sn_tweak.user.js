dbe3ea70a075e2bc0050b80379aee0ae1ea560b0
orionlee <orionlee@yahoo.com>
2021-08-08 11:53:07 -0700

    ASAS-SN : UI to support crossmatch (with VSX)

--- tess_asas_sn_tweak.user.js
+++ tess_asas_sn_tweak.user.js
@@ -3,7 +3,7 @@
 // @namespace   astro.tess
 // @match       https://asas-sn.osu.edu/variables*
 // @grant       GM_addStyle
-// @version     1.1
+// @version     1.2
 // @author      orionlee
 // @description
 // ==/UserScript==
@@ -39,7 +39,7 @@ tweakSearchUI();
 
 
 function isObjectDetailPage() {
-  return location.pathname.match(/^\/variables\/.+/);
+  return location.pathname.match(/^\/variables\/\w+/);
 }
 
 function tweakObjectDetail() {
@@ -53,3 +53,24 @@ a#variable-db-atlas-link {
 `);
 }
 tweakObjectDetail();
+
+// Extract IDs on object detail page for the purpose of cross matching.
+function extractIds() {
+  if (!isObjectDetailPage()) {
+    return;
+  }
+
+  const [asasSnId] = document.querySelector('h3').textContent.trim().match(/^ASASSN-V J[0-9-.+]+/) || [null];
+  const allwiseId = document.querySelector('.variable-star-data > div:nth-of-type(4) .row > div:nth-of-type(3) .star-data__value' )?.textContent;
+  const otherId = document.querySelector('.variable-star-data > div:nth-of-type(4) .row > div:nth-of-type(4) .star-data__value' )?.textContent?.replace(/^-$/, '');
+  const url = location.href;
+
+  document.body.insertAdjacentHTML('beforeend', `
+<div id="crossMatchCtr" style="position: fixed;right: 6px;top: 6px;padding: 4px 6px;background-color: rgba(255, 255, 0, 0.7);"
+     title="For cross-match's use. Shortcut: Alt-L">
+  <input id="crossMatchOut" value="" accessKey="L" onclick="this.select();" readonly>
+</div>
+  `);
+  document.getElementById('crossMatchOut').value = `${asasSnId}\t${otherId},WISEA ${allwiseId}\t${url}`
+}
+extractIds();

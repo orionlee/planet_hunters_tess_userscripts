3f6bad91681cfe93a67735869e6e8ebd26256482
orionlee <orionlee@yahoo.com>
2021-08-07 20:52:13 -0700

    SIMBAD: star type - wiki link updae

--- tess_simbad_tweak.user.js
+++ tess_simbad_tweak.user.js
@@ -7,7 +7,7 @@
 //                ^^^ links from SIMBAD in case coordinate-based search has multiple results
 // @grant       GM_addStyle
 // @noframes
-// @version     1.1.10
+// @version     1.1.12
 // @author      -
 // @description
 // @icon        https://panoptes-uploads.zooniverse.org/production/project_avatar/442e8392-6c46-4481-8ba3-11c6613fba56.jpeg
@@ -205,9 +205,12 @@ function simbadStarTypeToWikiLinkHtml(starType) {
       'Variable Star of Mira Cet type': 'Mira variable',
       'Variable Star of R CrB type': 'R Coronae Borealis variable',
       'Variable Star of alpha2 CVn type': 'Alpha2 Canum Venaticorum variable',
+      'T Tau-type Star': 'T Tauri star',
+      'Ellipsoidal variable Star': 'Rotating ellipsoidal variable',
       'Be Star': 'Be star',
       'Hot subdwarf': 'Hot subdwarf',
       'Wolf-Rayet Star': 'Wolf–Rayet star',
+      'Carbon Star': 'Carbon star',
     });
 
   // default is starType, and the mapping takes care of special cases

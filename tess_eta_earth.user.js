bd47db6f1315c6eed61bf47075e96ea1c36f5c03
orionlee <orionlee@yahoo.com>
2024-12-02 15:50:18 -0800

    [eta-earth] double-click to autoscale - only works in area right outside the plot

--- tess_eta_earth.user.js
+++ tess_eta_earth.user.js
@@ -4,7 +4,7 @@
 // @match       https://eta-earth.org/tess_fits_play.html
 // @noframes
 // @grant       GM_addStyle
-// @version     1.1
+// @version     1.2
 // @author      -
 // @description
 // @icon        https://eta-earth.org/favicon.ico
@@ -41,21 +41,20 @@ setTimeout(showStellarParams, 2000);
 
 //
 // mousewheel click to toggle zoom/pan, copied from tess_fli_tweak.user.js
+// added: double click to auto-scale
 //
 
+function getTool(name) {
+  // it selects the tool at the top, which is the tool for the LC
+  return document.querySelector(`.modebar-group > a[data-title="${name}"]`)
+}
+
+function isActive(toolEl) {
+  return toolEl?.classList?.contains("active");
+}
+
 function onMouseWheelToggleLCZoomPan(evt) {
-  function getTool(name) {
-    // it selects the tool at the top, which is the tool for the LC
-    return document.querySelector(`.modebar-group > a[data-title="${name}"]`)
-  }
 
-  function isActive(toolEl) {
-    return toolEl?.classList?.contains("active");
-  }
-
-  //
-  // main logic
-  //
   // we intercept middle click (button === 1) only
   if (evt.button !== 1) {
     return;
@@ -94,3 +93,29 @@ function useMouseWheelToToggleLCZoomPan() {
 }
 useMouseWheelToToggleLCZoomPan();
 
+
+function onDblClickAutoscale(evt) {
+  /// console.debug('dblclick', evt, evt.target);
+
+  if (!(
+    evt.target.classList.contains('svg-container') ||
+    ['RECT', 'G'].includes(evt.target.tagName.toUpperCase())
+       )) {
+    // Note: double click is not caught on svg (not in Chrome)
+    // so as a workaround,
+    // also track double click in the area right outside the plot (div.svg-container)
+    //
+    // tagName for svg elements in lower case in chrome,
+    // use upper case to avoid any potential browser incompatibility
+
+    // empirically a mouse down is caught by <rect> elements,
+    // but I also include the parent <g> to be safe.
+    return;
+  }
+
+  // case mouse wheel on the svg plot (for now we don't track if it's the LC's svg plot)
+  evt.preventDefault();
+
+  getTool("Autoscale")?.click();
+}
+document.addEventListener('dblclick', onDblClickAutoscale);

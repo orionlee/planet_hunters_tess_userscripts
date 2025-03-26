9d26c4cdcaa6daf71bf0d64a3ebff8f569caf1b5
orionlee <orionlee@yahoo.com>
2025-03-26 11:12:33 -0700

    [Vizier Gaia DR3 plot]: add controls to easily change period to 0.5x, 2x

--- tess_vizier_plot_gaiadr3_tweak.user.js
+++ tess_vizier_plot_gaiadr3_tweak.user.js
@@ -5,12 +5,42 @@
 //              ^^^ the iframe of Gaia DR3 Photometry plot. Parent URL in the form of:
 //                  https://cdsarc.cds.unistra.fr/vizier/vizgraph.gml?-s=I/355&-i=.graph_sql_epphot&*
 // @grant       GM_addStyle
-// @version     1.0.1
+// @version     1.1.0
 // @author      -
 // @description This script tweaks the inner iframe of the plot. See tess_vizier_plot_gaiadr3_outer_frame_tweak.user.js for the outer frame tweak.
 // @icon        https://cdsarc.cds.unistra.fr/favicon.ico
 // ==/UserScript==
 
+
+
+function changePeriod(factor) {
+  const perEl = document.querySelector('input#option_axis_x_period');
+  const curPer = parseFloat(perEl?.value);
+  if (!curPer) {
+    console.warn("changePeriod(): no current value. No-op")
+    return;
+  }
+  perEl.value = curPer * factor;
+}
+
+
+function addPeriodControlsUI() {
+  const perEl = document.querySelector('input#option_axis_x_period');
+  if (!perEl) {
+    console.warn('addPeriodControls(): Cannot find period input element. No-op');
+    return;
+  }
+
+  perEl.insertAdjacentHTML('afterend', `
+&nbsp;<button id="perHalfCtl">1/2 P</button>&nbsp;<button id="perDoubleCtl">2x P</button>
+`);
+  document.getElementById("perHalfCtl").onclick = () => {
+    changePeriod(0.5);
+  };
+  document.getElementById("perDoubleCtl").onclick = () => {
+    changePeriod(2);
+  };
+}
 
 
 function plotGmagOnlyInScatter() {
@@ -24,6 +54,8 @@ function plotGmagOnlyInScatter() {
 
 
 function initUI() {
+  addPeriodControlsUI();
+
   document.querySelector('#gadget_share').insertAdjacentHTML('afterbegin', `
 <button id="showGInDotsOnlyCtl">G scatter only</button>&nbsp;
 `);

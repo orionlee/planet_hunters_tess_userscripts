cff6c7a671b671b9ac8d3600a2f2556880bb7db4
orionlee <orionlee@yahoo.com>
2026-04-27 09:16:34 -0700

    [TESS GI] Updated auto-serach the proposals with hash #search=<term> - to keep up with the UI changes

--- tess_gi_proposals_list_tweak.user.js
+++ tess_gi_proposals_list_tweak.user.js
@@ -2,52 +2,32 @@
 // @name        TESS GI Proposal List Page Tweak
 // @namespace   astro.tess
 // @match       https://heasarc.gsfc.nasa.gov/docs/tess/approved-programs.html
-// @match       https://heasarc.gsfc.nasa.gov/docs/tess/approved-programs-em1.html
-// @match       https://heasarc.gsfc.nasa.gov/docs/tess/approved-programs-primary.html
 // @noframes
-// @version     1.0
+// @version     1.1
 // @author      -
 // @description
-// @icon        https://heasarc.gsfc.nasa.gov/favicon.ico
+// @icon        https://heasarc.gsfc.nasa.gov/docs/tess/images/logos/favicon.png
 // ==/UserScript==
 
 function autoFillSearchTerm() {
   function doFillValue(value, retryDelayMs) {
-    const inputEl = document.querySelector('input[type="search"]')
+    const inputEl = document.querySelector('input[type="search"]');
     if (inputEl) {
       inputEl.value = value;
+      inputEl.focus();
+      // change the focus to trigger the search
+      document.querySelector('a').focus();
       inputEl.focus();
     } else {
       setTimeout(() => doFillValue(value, retryDelayMs), retryDelayMs);
     }
-  };
-
-  if (location.pathname != '/docs/tess/approved-programs.html') {
-    return;
   }
 
-  const [, term] = location.search.match(/[?&]term=([^&]+)/) || [null, null];
+  const [, term] = location.hash.match(/[#]search=([^&]+)/) || [null, null];
   if (!term) {
     return;
   }
 
-  doFillValue(term, 2000);  // retry every 2s till the input is rendered.
+  doFillValue(term, 2000); // retry every 2s till the input is rendered.
 }
 autoFillSearchTerm();
-
-
-function addLinkToCurrentGIPage() {
-  if (
-    location.pathname == '/docs/tess/approved-programs-em1.html' ||
-    location.pathname == '/docs/tess/approved-programs-primary.html'
-  ) {
-    // The legacy pages no longer exist. Show link to the current URL.
-    document.body.insertAdjacentHTML('beforeend', `
-<div id="new_url_ctr" style="position: fixed; top: 30vh; left:2vw; padding: 12px; background: rgba(255, 255, 0, 0.7);">
-  <a href="https://heasarc.gsfc.nasa.gov/docs/tess/approved-programs.html" style="font-size: 120%;">TESS GI Approved Programs Page</a>
-</div>
-      `);
-  }
-}
-addLinkToCurrentGIPage();
-

804ebfe42d4d91dd671846ac457e2f4d95a15e12
orionlee <orionlee@yahoo.com>
2026-06-02 07:10:32 -0700

    [TESS GI] format sector target list CSVs to link to the proposals - requires another extensions to modify HTTP response headers   so that the browser will render the csv.

---
+++ tess_gi_target_lists_tweak.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         TESS GI Target List CSV UI tweaks
+// @namespace    astro.tess
+// @version      1.0.0
+// @description
+// @author
+// @match        https://heasarc.gsfc.nasa.gov/docs/tess/data/target_lists/*.csv
+// @icon         https://heasarc.gsfc.nasa.gov/docs/tess/images/logos/favicon.png
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // Note: browsers by default download the csv files.
+  // Need to use extension such as ModHeader to force browsers to render them as plain text
+  function addLinksToGIProposals() {
+    document.head.insertAdjacentHTML(
+      'beforeend',
+      `
+<style>
+  a {
+    padding: 0 1ch;
+  }
+</style>
+`,
+    );
+
+    document.querySelector('pre').innerHTML = document
+      .querySelector('pre')
+      .innerHTML.replaceAll(
+        /(G\d+)/g,
+        '<a href="https://heasarc.gsfc.nasa.gov/docs/tess/approved-programs.html#search=$1">$1</a>',
+      );
+  }
+  addLinksToGIProposals();
+})();

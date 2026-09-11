8594590118b3eb4cf1d6dfb52662b305aa5fa8ef
orionlee <orionlee@yahoo.com>
2026-09-11 09:13:16 -0700

    [ExoFOP, ExoMAST] include S106 start/stop times

--- tess_exomast_tce_tweak.user.js
+++ tess_exomast_tce_tweak.user.js
@@ -4,7 +4,7 @@
 // @match       https://exo.mast.stsci.edu/exomast_planet.html?planet=*
 // @grant       none
 // @noframes
-// @version     1.1.19
+// @version     1.1.20
 // @author      -
 // @description
 // @icon        https://panoptes-uploads.zooniverse.org/production/project_avatar/442e8392-6c46-4481-8ba3-11c6613fba56.jpeg
@@ -121,6 +121,7 @@ function bjtdToRelative(tBjtd) {
     [103, 4152.031471112816, 4178.200911241182],
     [104, 4179.18933292515, 4205.203494190147],
     [105, 4205.419061310566, 4233.059719822114],
+    [106, 4233.429416851645, 4261.875729892523],
   ]; // Note: update the one in tess_exofop_tweak.user.js too
 
   for (const row of sectorStartStopList) {
